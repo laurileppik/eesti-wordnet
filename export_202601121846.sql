796490d41eff1eda88088666532df25f58de4d18
laurileppik <laurile02@gmail.com>
2026-01-12 22:55:27 +0200

    export tables

---
+++ export_202601121846.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bbb93cae87c36abfd97e25a6f5dd16e735a41ad648dbba6e470bc95762c10a5d
+size 470322052
---
+++ tables.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e93c34380c23f771b994e12ac2de1a5b33f2ef3e27e8c073988e875fc5df8b26
+size 20011
