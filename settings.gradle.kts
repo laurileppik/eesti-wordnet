69fb688bdf9cf90dfb990c5a03174463ed590516
laurileppik <laurile02@gmail.com>
2026-01-13 18:29:18 +0200

    Lisa andmebaasi ühendus

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,6 @@
 rootProject.name = "eesti-wordnet"
+include("eesti-wordnet-db")
+include("eesti-wordnet-model")
 include("eesti-wordnet-rest")
+include("eesti-wordnet-services")
+include("eesti-wordnet-shared")
